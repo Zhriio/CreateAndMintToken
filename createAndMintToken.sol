f73ff1b5f34c221779affea594d6a8618fa7357b
Zhriio <144327617+Zhriio@users.noreply.github.com>
2024-12-02 10:45:20 +0800

    Create createAndMintToken.sol

---
+++ createAndMintToken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+
+contract DripToken is ERC20 {
+    
+    address public owner;
+
+    constructor() ERC20("DripToken","DPC") {
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(msg.sender == owner, "Minting is only accessible for the owner.");
+        _mint(to, amount);
+    }
+
+    function transferToken(address recipient, uint256 amount) external returns (bool) {
+        require(balanceOf(recipient)>= amount,"Insufficient amount to transer.");
+        return transfer(recipient, amount);
+    }
+
+    function burnToken(address account, uint256 amount) public {
+        require(balanceOf(account)>= amount,"Insufficient amount to burn.");
+        _burn(account, amount);
+    }
+}
